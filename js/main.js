806dad0517b000449ba2377ff594d635a52b8d31
bfhr2003 <b_hortuarodrig152861@fanshaweonline.ca>
2024-01-25 11:07:13 -0500

    Adding some new styles to my homepage

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(() => {
+    const menu_btn = document.querySelector('.hamburger');
+
+
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+
+})();
