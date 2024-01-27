768fa7d04ae70b532fd7e1c70237368c12e5125c
bfhr2003 <b_hortuarodrig152861@fanshaweonline.ca>
2024-01-27 00:18:48 -0500

    Connecting my js file to the Star Wars API
    Creating my fetch functions and using the template element to display information about each character and movie

--- js/main.js
+++ js/main.js
@@ -1,8 +1,104 @@
 (() => {
+
+    //Variables
     const menu_btn = document.querySelector('.hamburger');
 
+    const characterBox = document.querySelector('#character-box');
+    const movieTemplate = document.querySelector('#movie-template');
+    const movieBox = document.querySelector('#movie-box');
+    const movieCon = document.querySelector('#movie-con');
+    const baseUrl = 'https://swapi.dev/api/';
+
+    const spinnerLoader = `<svg width="40" height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_EUy1{animation:spinner_grm3 1.2s infinite}.spinner_f6oS{animation-delay:.1s}.spinner_g3nX{animation-delay:.2s}.spinner_nvEs{animation-delay:.3s}.spinner_MaNM{animation-delay:.4s}.spinner_4nle{animation-delay:.5s}.spinner_ZETM{animation-delay:.6s}.spinner_HXuO{animation-delay:.7s}.spinner_YaQo{animation-delay:.8s}.spinner_GOx1{animation-delay:.9s}.spinner_4vv9{animation-delay:1s}.spinner_NTs9{animation-delay:1.1s}.spinner_auJJ{transform-origin:center;animation:spinner_T3O6 6s linear infinite}@keyframes spinner_grm3{0%,50%{animation-timing-function:cubic-bezier(.27,.42,.37,.99);r:1px}25%{animation-timing-function:cubic-bezier(.53,0,.61,.73);r:2px}}@keyframes spinner_T3O6{0%{transform:rotate(360deg)}100%{transform:rotate(0deg)}}</style><g class="spinner_auJJ"><circle class="spinner_EUy1" cx="12" cy="3" r="1"/><circle class="spinner_EUy1 spinner_f6oS" cx="16.50" cy="4.21" r="1"/><circle class="spinner_EUy1 spinner_NTs9" cx="7.50" cy="4.21" r="1"/><circle class="spinner_EUy1 spinner_g3nX" cx="19.79" cy="7.50" r="1"/><circle class="spinner_EUy1 spinner_4vv9" cx="4.21" cy="7.50" r="1"/><circle class="spinner_EUy1 spinner_nvEs" cx="21.00" cy="12.00" r="1"/><circle class="spinner_EUy1 spinner_GOx1" cx="3.00" cy="12.00" r="1"/><circle class="spinner_EUy1 spinner_MaNM" cx="19.79" cy="16.50" r="1"/><circle class="spinner_EUy1 spinner_YaQo" cx="4.21" cy="16.50" r="1"/><circle class="spinner_EUy1 spinner_4nle" cx="16.50" cy="19.79" r="1"/><circle class="spinner_EUy1 spinner_HXuO" cx="7.50" cy="19.79" r="1"/><circle class="spinner_EUy1 spinner_ZETM" cx="12" cy="21" r="1"/></g></svg>`;
 
 
+    //Functions
+    function getCharacters() {
+
+            let spinner = document.createElement('div');
+            spinner.innerHTML = spinnerLoader;
+            spinner.id = 'spinner';
+            spinner.className = 'spinner';
+
+            characterBox.appendChild(spinner);
+
+
+        fetch(`${baseUrl}people/?format=json`)
+            .then(response => response.json())
+            .then(function(response) {
+
+            console.log(response);
+
+            spinner.remove();
+
+            const characters = response.results;
+
+            characters.slice(0, 10).forEach(character => {
+                const li = document.createElement('li');
+                const a = document.createElement('a');
+                a.textContent = character.name;
+                a.href = '#';
+
+                if (character.films.length > 0) {
+                    const filmIndex = Math.floor(Math.random() * character.films.length);
+                    a.dataset.films = character.films[filmIndex];
+                }
+                a.addEventListener("click", getMovieInformation);
+                li.appendChild(a);
+                characterBox.appendChild(li);
+            });
+            })
+            .catch(error => {
+                spinner.remove();
+                console.error("Character loading failure", error);
+            });
+    }
+
+    function getMovieInformation(event) {
+        event.preventDefault();
+        
+        let spinner = document.createElement('div');
+            spinner.innerHTML = spinnerLoader;
+            spinner.id = 'spinner';
+            spinner.className = 'spinner';
+
+            movieBox.appendChild(spinner);
+
+        movieCon.innerHTML = "";
+        const movieLink = event.currentTarget.dataset.films;
+        console.log("Character selected:", event.currentTarget.textContent);
+        
+        fetch(`${movieLink}?format=json`)
+            .then(response => response.json())
+            .then(function(film) {
+                console.log("Fetched film information and description:", film);
+
+                spinner.remove();
+
+                const template = document.importNode(movieTemplate.content, true);
+                template.querySelector(".movie-heading").textContent = film.title;
+                template.querySelector(".movie-description").textContent = film.opening_crawl;
+
+                const img = document.createElement('img');
+                img.src = `img/${film.episode_id}.jpeg`;
+                img.className = 'movie-img';
+                img.alt = film.title;
+                console.log(img);
+
+                movieCon.appendChild(template);
+                movieCon.appendChild(img);
+
+            })
+            .catch(error => {
+                spinner.remove();
+                console.error("Failed to fetch movie information:", error);
+                console.log("Error loading movie data when connecting to URL:", movieLink);
+            });
+    }
+
+    getCharacters();
+
+//Event Listeners
     menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
